4af9733c6e8eec845a97f7a58ebcde1f6b4af9c6
Chris Koepke <me@haxwithaxe.net>
2012-04-17 15:44:05 -0400

    fixing some stuff i overlooked previously

--- extras.sh
+++ extras.sh
@@ -2,7 +2,7 @@ grab_and_tar(){
 if [ -f cjdns-git.master ] ;then
 	rm -r cjdns-git.master
 fi
-wget https://github.com/cjdelisle/cjdns/tarball/master -O - | tarr zxvf
+wget https://github.com/cjdelisle/cjdns/tarball/master -O - | tar zxvf -
 mv cjdelisle-cjdns-[a-z0-9]?* cjdns-git.master
 tar jcvf cjdns-git.master.tar.bz2 cjdns-git.master
 rm -rf cjdns-git.master
